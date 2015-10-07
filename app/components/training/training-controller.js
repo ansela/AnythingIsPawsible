57429c40abb493e74611fc5cc1905dda98c73f65
ansela <ashley.ansell@gmail.com>
2015-10-07 16:45:52 -0400

    Added more content

--- app/components/training/training-controller.js
+++ app/components/training/training-controller.js
@@ -1,3 +1,3 @@
 angular.module('scotchApp').controller('trainingController', function($scope) {
-	$scope.message = 'We train your dog';
+	$scope.message = 'I would love to help you with any problems with your dogs, whether you want your dog to learn basic obedience or help with problem behaviors';
 });
