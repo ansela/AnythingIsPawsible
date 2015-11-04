6fbc1a9b99683f6c4fff2205ca8cb069f1c170c0
ansela <ashley.ansell@gmail.com>
2015-11-04 15:27:56 -0500

    Changed layout of home and contact pages

--- app/components/home/main-controller.js
+++ app/components/home/main-controller.js
@@ -2,4 +2,5 @@ angular
 	.module('scotchApp')
 	.controller('mainController', function($scope) {
 		$scope.message = 'hello world!';
+		$scope.lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis elit nulla. Curabitur quis pharetra arcu. Aliquam tincidunt ligula massa, eu dapibus velit sollicitudin eget. Nam interdum, sem vel iaculis lobortis, lectus nulla vehicula arcu, ac sagittis magna diam sit amet nibh. Sed tempor turpis metus, eu consequat urna ultricies quis.';
 });
